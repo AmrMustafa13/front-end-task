8e4fdd9c8c2e5d091c1d6f766d4b363b1ac59a97
AmrMustafa13 <amrmostafa1311@gmail.com>
2022-12-08 17:59:05 +0200

    form validation

--- signup.js
+++ signup.js
@@ -5,17 +5,6 @@ const email = document.querySelector(".email");
 const password = document.querySelector(".password");
 const password2 = document.querySelector(".password2");
 
-// check email validation
-function checkEmail(input) {
-  const pattern =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (input.value.trim().match(pattern)) {
-    showSuccess(input);
-  } else {
-    showError(input, `Email is not valid`);
-  }
-}
-
 // show error function
 function showError(input, message) {
   const formControler = input.parentElement;
@@ -32,17 +21,6 @@ function showSuccess(input) {
   formControler.classList.add("success");
 }
 
-// check required inputs
-function checkRequired(input) {
-  input.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input.id)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
-
 // get the capitalized field name
 function getFieldName(input) {
   return input.charAt(0).toUpperCase() + input.slice(1);
@@ -53,12 +31,12 @@ function checkLength(input, min, max) {
   if (input.value.length < min) {
     showError(
       input,
-      `${getFieldName(input.id)} must be at least ${min} characters`
+      `${getFieldName(input.classList[0])} must be at least ${min} characters`
     );
   } else if (input.value.length > max) {
     showError(
       input,
-      `${getFieldName(input.id)} must be less than ${max} characters`
+      `${getFieldName(input.classList[0])} must be less than ${max} characters`
     );
   } else {
     showSuccess(input);
@@ -69,39 +47,40 @@ function checkLength(input, min, max) {
 function checkPasswordMatch(input1, input2) {
   if (input2.value !== input1.value) {
     showError(input2, `Passwords do not match`);
+  } else {
+    showSuccess(input2);
   }
 }
 
 // add the event listener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  checkRequired([userName, email, password, password2]);
-  checkEmail(email);
   checkLength(userName, 5, 15);
   checkLength(password, 8, 30);
   checkPasswordMatch(password, password2);
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const dataObj = {
-    username: userName.value,
-    email: email.value,
-    password: password.value,
-    password2: password2.value,
-  };
-  fetch(`https://goldblv.com/api/hiring/tasks/register`, {
-    method: "POST",
-    body: JSON.stringify(dataObj),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((e) => {
-      localStorage.setItem("user data", JSON.stringify(dataObj));
-      window.location.href = "./succeed.html";
+  const errors = Array.from(
+    document.querySelectorAll("main .content .form-controler.error")
+  );
+  if (errors.length === 0) {
+    const dataObj = {
+      username: userName.value,
+      email: email.value,
+      password: password.value,
+      password2: password2.value,
+    };
+    fetch(`https://goldblv.com/api/hiring/tasks/register`, {
+      method: "POST",
+      body: JSON.stringify(dataObj),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .catch((e) => {
-      console.log(e);
-    });
+      .then((e) => {
+        localStorage.setItem("user data", JSON.stringify(dataObj));
+        window.location.href = "./succeed.html";
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 });
